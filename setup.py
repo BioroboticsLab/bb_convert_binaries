30bff2ea1849e2cd6db21e24d1636f8759373004
gitmirgut <gitmirgut@users.noreply.github.com>
2017-07-17 12:12:24 +0200

    build up structure.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from pip.req import parse_requirements
+from setuptools import setup
+
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+dep_links = [str(req_line.url) for req_line in install_reqs]
+
+setup(
+    name='bb_convert_binaries',
+    version='0.0.0.dev1',
+    description='This package is used to convert the bb_binary data.',
+    long_description='',
+    url='https://github.com/gitmirgut/bb_convert_binaries',
+    install_requires=reqs,
+    dependency_links=dep_links,
+    author='gitmirgut',
+    author_email="gitmirgut@users.noreply.github.com",
+    packages=['bb_convert_binaries'],
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5'
+    ],
+)
