98852b9188e81a357373065007a383a41f44c926
gitmirgut <gitmirgut@users.noreply.github.com>
2017-08-18 15:48:53 +0200

    Add root dir replacement function.

--- bb_convert_binaries/helpers.py
+++ bb_convert_binaries/helpers.py
@@ -25,3 +25,17 @@ def get_default_path_new_scheme():
     path_schema = os.path.join(os.path.dirname(__file__), 'bb_binary_schemas',
                                'bb_binary_scheme_new.capnp')
     return path_schema
+
+
+def replace_root_dir(old_root_dir, new_root_dir, old_file_path):
+    """Return new file path, which is a combination of new_root_dir and old_file_path.
+
+    Args:
+        old_root_dir(str): Root dir to be replaced.
+        new_root_dir(str): This is new root dir, which would replace old root dir.
+        old_file_path(str): Path with old root dir.
+
+    Returns: new file path.
+    """
+    rel_file_path = old_file_path[len(old_root_dir) + 1:]
+    return os.path.join(new_root_dir, rel_file_path)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -9,3 +9,14 @@ def test_get_default_path_old_scheme():
 
 def test_get_default_path_new_scheme():
     assert os.path.exists(helpers.get_default_path_new_scheme())
+
+
+def test_replace_root_dir():
+    old_root_dir = '/mnt/storage/beesbook-data-clean/pipeline_data/repo_season_2016_fixed'
+    new_root_dir = '/this/is/the/new/root/dir'
+    old_file_path = '/mnt/storage/beesbook-data-clean/pipeline_data/repo_season_2016_fixed/2016/' \
+                    '08/01/09/20/Cam_0_2016-08-01T09:24:28.524594Z--2016-08-01T09:30:08.371296Z.bbb'
+    new_path = helpers.replace_root_dir(old_root_dir, new_root_dir, old_file_path)
+    res_path = '/this/is/the/new/root/dir/2016/08/01/09/20/' \
+               'Cam_0_2016-08-01T09:24:28.524594Z--2016-08-01T09:30:08.371296Z.bbb'
+    assert new_path == res_path
